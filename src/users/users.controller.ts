a7f9cbf2b3e7fa5321a7cc6d90b4c4684a7821bc
Mateus Pedroso <mateuspsilva.dev@gmail.com>
2022-12-27 10:53:40 -0300

    add dto to route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,6 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiBody } from '@nestjs/swagger';
+import { UserDto } from './dtos/user.dto';
 import { User } from './interfaces/user.interface';
 import { UsersService } from './users.service';
 
@@ -7,7 +9,8 @@ export class UsersController {
   constructor(private readonly userService: UsersService) {}
 
   @Post()
-  create(@Body() user: User): User {
+  @ApiBody({ type: UserDto })
+  create(@Body() user: UserDto): User {
     return this.userService.create(user);
   }
 
